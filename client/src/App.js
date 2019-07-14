a7c146feedc2191dea76f2ef94a6462705d6f3e6
Andrei Kassiantchouk <akass1122@gmail.com>
2019-07-14 00:49:36 -0700

    V45

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import setAuthToken from './utils/setAuthToken';
 import './App.css';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -31,6 +33,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
