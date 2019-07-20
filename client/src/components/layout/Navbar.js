ab29742dbdde32af04679d958a28501e7b4a0e8e
Andrei Kassiantchouk <akass1122@gmail.com>
2019-07-19 17:20:10 -0700

    V60half

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,9 +8,12 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
-        <li>
-          <Link to='/profiles'>Developers</Link>
-        </li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
+      <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <i className='fas fa-user' />
           <span className='hide-sm' />
--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
-// import ProfileAbout from './ProfileAbout';
-// import ProfileExperience from './ProfileExperience';
-// import ProfileEducation from './ProfileEducation';
-// import ProfileGithub from './ProfileGithub';
+import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -18,7 +18,9 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById, match.params.id]);
+  }, [getProfileById]);
+
+  //, match.params.id
 
   return (
     <Fragment>
@@ -38,17 +40,17 @@ const Profile = ({
             )}
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
-            {/* <ProfileAbout profile={profile} /> */}
+            <ProfileAbout profile={profile} />
             <div className='profile-exp bg-white p-2'>
               <h2 className='text-primary'>Experience</h2>
               {profile.experience.length > 0 ? (
                 <Fragment>
-                  {profile.experience.map(experience => ({
-                    /* <ProfileExperience
+                  {profile.experience.map(experience => (
+                    <ProfileExperience
                       key={experience._id}
                       experience={experience}
-                    /> */
-                  }))}
+                    />
+                  ))}
                 </Fragment>
               ) : (
                 <h4>No experience credentials</h4>
@@ -59,22 +61,21 @@ const Profile = ({
               <h2 className='text-primary'>Education</h2>
               {profile.education.length > 0 ? (
                 <Fragment>
-                  {profile.education.map(education => ({
-                    /* <ProfileEducation
+                  {profile.education.map(education => (
+                    <ProfileEducation
                       key={education._id}
                       education={education}
-                    /> */
-                  }))}
+                    />
+                  ))}
                 </Fragment>
               ) : (
                 <h4>No education credentials</h4>
               )}
             </div>
 
-            {profile.githubusername &&
-              {
-                /* <ProfileGithub username={profile.githubusername} /> */
-              }}
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}
