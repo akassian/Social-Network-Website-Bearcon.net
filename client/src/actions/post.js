8ebb646972fa02ae1031b92c4bde27c055507f54
Andrei Kassiantchouk <akass1122@gmail.com>
2019-07-21 19:30:32 -0700

    final v1

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -14,7 +14,7 @@ import {
 // Get posts
 export const getPosts = () => async dispatch => {
   try {
-    const res = await axios.get('http://localhost:5000/api/posts');
+    const res = await axios.get('/api/posts');
 
     dispatch({
       type: GET_POSTS,
@@ -31,7 +31,7 @@ export const getPosts = () => async dispatch => {
 // Add like
 export const addLike = id => async dispatch => {
   try {
-    const res = await axios.put(`http://localhost:5000/api/posts/like/${id}`);
+    const res = await axios.put(`/api/posts/like/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
@@ -48,7 +48,7 @@ export const addLike = id => async dispatch => {
 // Remove like
 export const removeLike = id => async dispatch => {
   try {
-    const res = await axios.put(`http://localhost:5000/api/posts/unlike/${id}`);
+    const res = await axios.put(`/api/posts/unlike/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
@@ -65,7 +65,7 @@ export const removeLike = id => async dispatch => {
 // Delete post
 export const deletePost = id => async dispatch => {
   try {
-    await axios.delete(`http://localhost:5000/api/posts/${id}`);
+    await axios.delete(`/api/posts/${id}`);
 
     dispatch({
       type: DELETE_POST,
@@ -90,11 +90,7 @@ export const addPost = formData => async dispatch => {
   };
 
   try {
-    const res = await axios.post(
-      'http://localhost:5000/api/posts',
-      formData,
-      config
-    );
+    const res = await axios.post('/api/posts', formData, config);
 
     dispatch({
       type: ADD_POST,
@@ -113,7 +109,7 @@ export const addPost = formData => async dispatch => {
 // Get post
 export const getPost = id => async dispatch => {
   try {
-    const res = await axios.get(`http://localhost:5000/api/posts/${id}`);
+    const res = await axios.get(`/api/posts/${id}`);
 
     dispatch({
       type: GET_POST,
@@ -137,7 +133,7 @@ export const addComment = (postId, formData) => async dispatch => {
 
   try {
     const res = await axios.post(
-      `http://localhost:5000/api/posts/comment/${postId}`,
+      `/api/posts/comment/${postId}`,
       formData,
       config
     );
@@ -159,9 +155,7 @@ export const addComment = (postId, formData) => async dispatch => {
 // Delete comment
 export const deleteComment = (postId, commentId) => async dispatch => {
   try {
-    await axios.delete(
-      `http://localhost:5000/api/posts/comment/${postId}/${commentId}`
-    );
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
 
     dispatch({
       type: REMOVE_COMMENT,
