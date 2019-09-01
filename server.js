83b4faca5497e7faac96f1ad8b8d547a1bd1b483
Andrei Kassiantchouk <akass1122@gmail.com>
2019-09-01 14:26:45 -0700

    on AWS. server.js has if (process.env.NODE_ENV==='production'). Here we manually setup in AWS Console env variable NODE_ENV to production

--- server.js
+++ server.js
@@ -23,13 +23,14 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 //Serve static assets in production
-//if (process.env.NODE_ENV === 'production') {
-//Set static folder
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-//}
+
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // UPDATE ALL DOCUMENTS IN DATABASE BY ADDING SOME FIELDS
 // Callback function, numAffected  is number of affected documents and other info
